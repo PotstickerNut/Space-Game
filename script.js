669c1e3cecb5e6a047d39a5ae11d3aeb1b628113
Janette Campbell <campbjp@hotmail.com>
2022-05-07 23:07:01 -0400

    added css

--- script.js
+++ script.js
@@ -17,16 +17,6 @@ class Ship {
 }
 
 const generateShip = () => {
-  //call elements
-  const mainWrapper = document.querySelector(".main-wrapper");
-  // create elements
-  const div = document.createElement("div");
-
-  // append elements
-  mainWrapper.appendChild(div);
-
-  // add class to elements
-  div.className = "game-status";
   if (count > 0) {
     const hull = Math.trunc(Math.random() * 3) + 3;
     const firepower = Math.trunc(Math.random() * 3) + 2;
@@ -35,6 +25,17 @@ const generateShip = () => {
     count--;
     return alienGenShip;
   } else {
+    //call elements
+    const mainWrapper = document.querySelector(".main-wrapper");
+    // create elements
+    const div = document.createElement("div");
+
+    // append elements
+    mainWrapper.appendChild(div);
+
+    // add class to elements
+    div.className = "game-status you-won";
+
     console.log("You won!");
     div.textContent = "You won!";
 
@@ -44,30 +45,32 @@ const generateShip = () => {
 
 // Alien fire
 const alienFire = () => {
-  //call elements
-  const mainWrapper = document.querySelector(".main-wrapper");
-  // create elements
-  const div = document.createElement("div");
-
-  // append elements
-  mainWrapper.appendChild(div);
-
-  // add class to elements
-  div.className = "game-status";
-
   if (playing) {
+    //call elements
+    const mainWrapper = document.querySelector(".main-wrapper");
+    // create elements
+    const div1 = document.createElement("div");
+
+    // append elements
+    mainWrapper.appendChild(div1);
+
+    // add class to elements
+    div1.className = "game-status alien-fire";
     if (ussHelloWorld.hull > 0) {
       if (Math.random() < alienShip.accuracy) {
         console.log("USS Hello World has been hit!");
-        div.textContent = "USS Hello World has been hit!";
         ussHelloWorld.hull -= alienShip.firepower;
+        div1.textContent = `USS Hello World has been hit! Hull: ${ussHelloWorld.hull}`;
       } else {
         console.log("Missed USS Hello World target!");
-        div.textContent = "Missed USS Hello World target!";
+        div1.textContent = "Missed USS Hello World target!";
       }
     } else {
       console.log("You loose!");
-      div.textContent = "You loose!";
+      const div2 = document.createElement("div");
+      mainWrapper.appendChild(div2);
+      div2.className = "game-status you-loose";
+      div2.textContent = "You loose!";
       playing = false;
     }
   }
@@ -75,24 +78,24 @@ const alienFire = () => {
 
 // USS fire
 const ussFire = () => {
-  //call elements
-  const mainWrapper = document.querySelector(".main-wrapper");
-  // create elements
-  const div1 = document.createElement("div");
-  const div2 = document.createElement("div");
-
-  // append elements
-  mainWrapper.appendChild(div1);
-  mainWrapper.appendChild(div2);
-
-  // add class to elements
-  div1.className = "game-status";
-  div2.className = "game-status";
   if (playing) {
+    //call elements
+    const mainWrapper = document.querySelector(".main-wrapper");
+    // create elements
+    const div1 = document.createElement("div");
+    const div2 = document.createElement("div");
+
+    // append elements
+    mainWrapper.appendChild(div1);
+    mainWrapper.appendChild(div2);
+
+    // add class to elements
+    div1.className = "game-status uss-fire";
+    div2.className = "game-status generate-ship";
     if (Math.random() < ussHelloWorld.accuracy) {
       console.log("Alien has been hit!");
-      div1.textContent = "Alien has been hit!";
       alienShip.hull -= ussHelloWorld.firepower;
+      div1.textContent = `Alien has been hit! Hull: ${alienShip.hull}`;
       if (alienShip.hull <= 0) {
         console.log("Ship has gone down!");
         div2.textContent = "Ship has gone down!";
@@ -108,11 +111,13 @@ const ussFire = () => {
 
 const reset = () => {
   const divs = document.querySelectorAll(".game-status");
-  console.log(divs);
+
   divs.forEach((div) => {
     div.remove();
   });
+  console.log(divs);
   playing = true;
+  count = 6;
   alienShip = generateShip();
   ussHelloWorld = new Ship(20, 5, 0.7);
 };
